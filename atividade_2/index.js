0b6c28feea04c249e1a9da5477a8d8f10eb4caad
Bruno <brunobningles@gmail.com>
2020-06-21 19:45:33 -0300

    Exercicio(atividade_2): Criar uma aplicação de prevenção de COVID

---
+++ atividade_2/index.js
@@ -0,0 +1,13 @@
+let rs = require('readline-sync');
+
+let questionOne = String(rs.question('Está usando mascara quando precisa sair de casa? '));
+let questionTwo = String(rs.question('Está lavando as mãos frequentemente? '));
+let confirm = 'Sim';
+let not = 'Nao';
+
+if(questionOne == confirm && questionTwo == confirm) {
+    console.log('Meus parabens!! continue assim, vamos todos juntos acabar com a COVID-19');
+} else if(questionOne == not && questionTwo == not) {
+    console.log('Olha só enquanto todos lutam tem um ser que acha que nunca pegará, isso é muito feio, você não está contribuindo para acabar com a COVID-19 e, sim está ajudando para que o mesmo continue crescendo cada vez mais! Vamos se juntar ao restante da turma e fazer tudo direitinho?')
+}else {
+    console.log('Dados inconsistentes');
