6d57accb2a180375e5cd5cf060dc96e44b0cea66
Bruno <brunobningles@gmail.com>
2020-06-22 20:28:10 -0300

    Exercicio(atividade_1): Refatorando Exercicio!

--- atividade_1/index.js
+++ atividade_1/index.js
@@ -2,16 +2,35 @@ let rs = require('readline-sync');
 
 let number = Number(rs.question('Qual o numero do seu voo? '));
 let fileira = String(rs.question('Qual fileira deseja sentar? '));
+
+/* 
 let letra1 = 'A';
 let letra2 = 'B';
 let letra3 = 'C';
 let letra4 = 'D';
 let letra5 = 'E';
-let letra6 = 'F'; 
+let letra6 = 'F';
+*/
 
-if(fileira == letra1 || fileira == letra2 || fileira == letra3 || fileira == letra4 || fileira == letra5 || fileira == letra6) {
-    console.log('O numero do seu voo é ' + number + ' e a fileira é ' + fileira);
-    console.log('Boa Viagem!');
-}else {
-    console.log('Dados inconsistentes');
+switch(fileira){
+    case 'A':
+        console.log('A fileira que irá sentar é a A');
+        break;
+    case 'B':
+        console.log('A fileira que irá sentar é a B');
+        break;
+    case 'C':
+        console.log('A fileira que irá sentar é a C');
+        break;
+    case 'D':
+        console.log('A fileira que irá sentar é a D');
+        break;
+    case 'E':
+        console.log('A fileira que irá sentar é a E');
+        break;
+    case 'F':
+        console.log('A fileira que irá sentar é a F');
+        break;
+    default:
+        console.log('Dados invalidos');
 }
